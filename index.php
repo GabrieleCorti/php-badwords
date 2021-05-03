4c56f79f6df9a7b71b73da0040f7e9b076133286
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-05-03 15:32:47 +0200

    fine es

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
     $badWord = $_GET["badWord"];  
-    $txt = "un lungo paragrafo con delle parole divertenti, peculiari e alquanto proibite. Le parole possono essere molte e di diversi tipi, loquace presempio, non molto usata come parola. Erpetologo d'altro canto ha un suono divertente quanto complicato è il suo significato di esperto di lettere, infine $badWord; la aprola si spiega da sola";
+    $txt = "un lungo paragrafo con delle parole divertenti, peculiari e alquanto proibite. Le parole possono essere molte e di diversi tipi, loquace presempio, non molto usata come parola. Erpetologo d'altro canto ha un suono divertente quanto complicato è il suo significato di esperto di lettere, infine $badWord; la aprola si spiega da sola.";
+    $txtReplaced = str_replace($badWord, "***", $txt);
     $lunhgezza = strlen($txt);
-    str_replace($badWord, "***", $txt);
+    /* var_dump(str_replace($badWord, "***", $txt)); */
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +14,7 @@
     <title>Document</title>
 </head>
 <body>
-  <p><?php echo $txt; ?></p>
+  <p><?php echo $txtReplaced; ?></p>
   <p><?php echo "il paragrafo è lungo: $lunhgezza caratteri compresi spazzi"?></p>  
 </body>
 </html>
